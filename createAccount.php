ec31495f305cb3cea99304226f3294e7033772bb
Jylen02 <129941530+Jylen02@users.noreply.github.com>
2023-04-18 08:35:16 +0200

    Add files via upload

---
+++ createAccount.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Cuisine pour les nuls</title>
+    <link rel="stylesheet"
+    type="text/css"
+    href="recette.css">
+    <link rel="stylesheet"
+    type="text/css"
+    href="createAccount.css">
+    <script src="createAccount.js" type="text/javascript">
+    </script>
+</head>
+<body>
+    <div>
+        <a href="home.php" id="retourAcceuil">
+        <- Acceuil
+        </a>
+    </div>
+    <div id="corps">
+        <div id="titre">
+            S'incrire
+        </div>
+        <div>
+            <form action="User.php" method="POST">
+                <div class="SignUp">
+                    <label for="email">
+                        E-mail : 
+                    </label>
+                    </br>
+                    <input type="text" id="email" name="email" autofocus="autofocus" required>
+                    </br>
+                    <label for="username">
+                        Identifiant : 
+                    </label>
+                    </br>
+                    <input type="text" id="username" name="username" required>
+                    </br>
+                    <label for="password">
+                        Mot de passe : 
+                    </label>
+                    </br>
+                    <input type="password" id="password" name="password" class=password required>
+                    </br>
+                    <label for="confirmPassword">
+                        Confirmer le mot de passe : 
+                    </label>
+                    </br>
+                    <input type="password" id="confirmPassword" name="confirmPassword" class=password oninput="checkPass()" required>
+                    </br>
+                    <div id="message"></div>
+                    <button type="submit" id="submit" onclick=ajouter_utilisateur()>Continuer</button>
+                </div>
+            </form>
+        </div>
+        <div class="SignIn">
+            <a href="login.php" id="SignIn">
+                Déjà un compte ? Connectez-vous
+            </a> 
+        </div>
+    </div>
+</body>
+</html>
---
+++ home.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>   
+<head>
+    <title>Cuisine pour les nuls</title>
+    <link rel="stylesheet"
+    type="text/css"
+    href="recette.css">
+    <link rel="stylesheet"
+    type="text/css"
+    href="home.css">
+</head>
+<body>
+    <div id="top">
+        <div>
+            <button aria-expanded="false" aria-haspopup="menu" aria-label="Menu"></button>
+        </div>
+        <div>
+            <input type="text" placeholder="Rechercher une recette" id=barreRecherche>
+        </div>
+        <div>
+            <a href="login.php" id="login" class="Connexion">
+                Connexion
+            </a>
+            <a href="createAccount.php" id="createAccount" class="Connexion">
+                Cr&eacute;er un compte
+            </a>
+        </div>
+    </div>
+</body>
+</html>
