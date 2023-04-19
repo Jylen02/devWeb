48027532387f4a0a7843116f05a2ea948a3aa818
ChristopheCai <caichristo@cy-tech.fr>
2023-04-19 10:55:33 +0200

    Update
    Répartition des fichiers dans des dossiers + modification de leur chemin d'accès

--- php/login.php
+++ php/login.php
@@ -1,18 +1,17 @@
 <!DOCTYPE html>
-<html>
+<html lang="fr">
 <head>
-    <title>Cuisine pour les nuls</title>
+    <?php
+        include_once("Head.php");
+    ?>
     <link rel="stylesheet"
     type="text/css"
-    href="recette.css">
-    <link rel="stylesheet"
-    type="text/css"
-    href="login.css">
+    href="../css/login.css">
 </head>
 <body>
     <div>
         <a href="home.php" id="retourAcceuil">
-        <- Accueil
+        ← Accueil
         </a>
     </div>
     <div id="corps">
--- php/settings.php
+++ php/settings.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Settings</title>
-    <link rel="stylesheet" type="text/css" href="settings.css">
+    <link rel="stylesheet" type="text/css" href="../css/settings.css">
     <script type="text/javascript" src="settings.js"> </script>
 </head>
 <body class=" bgcolorWhite" onload="click1(['lucas','Guillot','test1','e-mail','test1.com'])">
