6ad6467f89a05a1aa5083df363d5f8650ea43440
AbdL2n <ouaaliabde@cy-tech.fr>
2023-06-02 16:15:44 +0200

    2 listes

--- php/recette/consulteAllRecipe.php
+++ php/recette/consulteAllRecipe.php
@@ -25,9 +25,9 @@
     }
     ?>
 
-    <h1 class="center">Liste des recettes</h1>
+    <h1 class="center">Liste des recettes <span class="rouge">en cours de validation</span></h1>
 
-    <table cellspacing=10 cellpadding=10>
+    <table cellspacing=10 cellpadding=10 >
         <tr>
             <th>Nom de l'utilisateur</th>
             <th>Nom de la recette</th>
@@ -41,6 +41,41 @@
             </tr>
         <?php endforeach; ?>
     </table>
+
+
+
+
+    <?php
+    // Récupération des recettes depuis la table "recipe"
+    $sql = "SELECT id, idUser, name FROM recipe WHERE valid = 1";
+    $result = $connexion->query($sql);
+
+    // Vérification si des recettes ont été trouvées
+    if ($result && $result->num_rows > 0) {
+        $recettes = $result->fetch_all(MYSQLI_ASSOC);
+    } else {
+        $recettes = array();
+    }
+    ?>
+
+    <h1 class="center">Liste des recettes <span class="vert">en lignes</span></h1>
+
+    <table cellspacing=10 cellpadding=10 >
+        <tr>
+            <th>Nom de l'utilisateur</th>
+            <th>Nom de la recette</th>
+        </tr>
+        <?php foreach ($recettes as $recette): ?>
+            <tr>
+                <td>
+                    <?php echo $recette['idUser']; ?>
+                </td>
+                <td><a href="consulteRecipe.php?id=<?php echo $recette['id']; ?>"><?php echo $recette['name']; ?></a></td>
+            </tr>
+        <?php endforeach; ?>
+    </table>
+    
+    
 
     <footer>
         <div class="center">
