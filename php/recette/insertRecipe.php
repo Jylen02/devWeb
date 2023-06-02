1cdd059572bababca42e7a47ed103d5c2a3480fe
Jylen02 <saelenjere@cy-tech.fr>
2023-06-02 15:12:22 +0200

    publishedRecipe

--- php/recette/insertRecipe.php
+++ php/recette/insertRecipe.php
@@ -4,11 +4,13 @@ include_once("../database.php");
 // Vérifier si l'utilisateur est connecté
 if (!isset($_SESSION['idUser'])) {
     header("Location: ../profil/login.php");
+    exit();
 }
 // Vérification si l'utilisateur est l'administrateur
 if ($_SESSION['idUser'] === 'admin') {
     // Rediriger l'administrateur vers la page de consultation des recettes
     header("Location: consulteRecipe.php");
+    exit();
 }
 // Vérifier si le formulaire a été soumis
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
@@ -19,6 +21,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $fornumber = $_POST['fornumber'];
     $time = $_POST['time'];
     $difficulty = $_POST['difficulty'];
+    $date = date("Y-m-d");
 
     // Vérifier si un fichier a été téléchargé
     if (isset($_FILES['image']) && $_FILES['image']['error'] === UPLOAD_ERR_OK) {
@@ -31,42 +34,41 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $image = null;
     }
 
-    // Connexion à la base de données et insertion des données dans la table "recipeinprocess"
+    // Connexion à la base de données et insertion des données dans la table "recipe"
 
-    try {
-        // Insérer les données de la recette dans la table "recipeinprocess"
-        $insertRecipeProcess = "INSERT INTO recipeinprocess (name, description, image, idUser, fornumber, time, difficulty) VALUES (?, ?, ?, ?, ?, ?, ?)";
-        $stmt = $connexion->prepare($insertRecipeProcess);
-        $stmt->bind_param("ssbssss", $name, $description, $image, $idUser, $fornumber, $time, $difficulty);
-        $stmt->execute();
 
-        if ($stmt->affected_rows > 0) {
-            // Une nouvelle ligne a été insérée avec succès
-            echo "Nouvelle ligne insérée dans la table recipeinprocess.";
-        } else {
-            // Aucune ligne insérée (erreur ou aucune modification)
-            echo "Aucune ligne insérée dans la table recipeinprocess." . $connexion->error;
+    // Insérer les données de la recette dans la table "recipe"
+    $insertRecipe = "INSERT INTO recipe (name, description, image, publishedDate, idUser, fornumber, time, difficulty) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+    $stmtRecipe = $connexion->prepare($insertRecipe);
+    $stmtRecipe->bind_param("ssbsssss", $name, $description, $image, $date, $idUser, $fornumber, $time, $difficulty);
+    $stmtRecipe->execute();
 
-        }
+    if ($stmtRecipe->affected_rows > 0) {
+        // Une nouvelle ligne a été insérée avec succès
+        echo "Nouvelle ligne insérée dans la table recipe.";
         // Récupérer l'ID de la recette insérée
-        $recipeId = $connexion->insert_id;
-
+        $idRecipe = $stmtRecipe->insert_id;
         // Insérer les ingrédients de la recette dans la table "product"
         $ingredients = $_POST['ingredient_name'];
         $quantities = $_POST['ingredient_quantity'];
-
         for ($i = 0; $i < count($ingredients); $i++) {
             $ingredient = $ingredients[$i];
             $quantity = $quantities[$i];
-
             $insertProduct = "INSERT INTO product (idRecipe, name, amount) VALUES (?, ?, ?)";
-            $stmt = $connexion->prepare($insertProduct);
-            $stmt->bind_param("sss", $recipeId, $ingredient, $quantity);
-            $stmt->execute();
+            $stmtProduct = $connexion->prepare($insertProduct);
+            $stmtProduct->bind_param("sss", $idRecipe, $ingredient, $quantity);
+            $stmtProduct->execute();
+            if ($stmtProduct->affected_rows > 0) {
+                // Une nouvelle ligne a été insérée avec succès
+                echo "Nouvelle ligne insérée dans la table recipe.";
+            } else {
+                // Aucune ligne insérée (erreur ou aucune modification)
+                echo "Aucune ligne insérée dans la table recipe." . $stmtProduct->error;
+            }
         }
 
-        // Calculer et mettre à jour le prix dans la table "recipeinprocess"
-        $updatePriceRecipeProcess = "UPDATE recipeinprocess 
+        // Calculer et mettre à jour le prix dans la table "recipe"
+        $updatePriceRecipe = "UPDATE recipe 
             SET price = ( 
                     SELECT SUM( productlist.price * product.amount ) 
                     FROM product 
@@ -74,30 +76,20 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                     WHERE product.idRecipe = ? 
                         ) 
             WHERE id = ?;";
-        $stmt = $connexion->prepare($updatePriceRecipeProcess);
-        if (!$stmt) {
-            // Afficher l'erreur de la base de données
-            echo "Erreur de préparation de la requête : " . $connexion->error;
-            exit;
-        }
-        $stmt->bind_param("ss", $recipeId, $recipeId);
-        $stmt->execute();
-
-        $connexion->commit();
-
-        // Succès de l'enregistrement
-        echo "<script>alert('La publication s'est faite avec succès!');</script>";
-        // Redirection vers une page de confirmation ou une autre action souhaitée
-        if (isset($_SESSION['idUser'])) {
-            echo "<script>window.location.href = '../accueil/home.php?success=1';</script>";
-        } else {
-            echo "<script>window.location.href = '../accueil/home.php?success=0';</script>";
-        }
-    } catch (PDOException $e) {
-        // Erreur lors de l'enregistrement
-        $connexion->rollback();
-        echo "<script>alert('Une erreur est survenue lors de l'enregistrement de la publication. Veuillez réessayer.');</script>";
+        $stmtPriceRecipe = $connexion->prepare($updatePriceRecipe);
+        $stmtPriceRecipe->bind_param("ss", $recipeId, $recipeId);
+        $stmtPriceRecipe->execute();
+        header("Location: ../accueil/home.php?success=1");
+    } else {
+        // Aucune ligne insérée (erreur ou aucune modification)
+        echo "Aucune ligne insérée dans la table recipe." . $stmtRecipe->error;
+        header("Location: publishedRecipe.php");
     }
+    $connexion->commit();
+
+    // Succès de l'enregistrement
+    echo "<script>alert('La publication s'est faite avec succès!');</script>";
+    // Redirection vers une page de confirmation ou une autre action souhaitée
 
     exit;
 }
--- php/recette/publishedRecipe.php
+++ php/recette/publishedRecipe.php
@@ -1,123 +1,20 @@
 <!DOCTYPE html>
-<html lang="fr">  
+<html lang="fr">
+
 <head>
     <?php
     include_once("../Head.php");
+    include_once("../database.php");
+    session_start();
     ?>
     <link rel="stylesheet" type="text/css" href="../../css/home.css">
     <link rel="stylesheet" type="text/css" href="../../css/imageSetting.css">
     <link rel="stylesheet" type="text/css" href="../../css/publishedRecipe.css">
     <script src="../../js/home.js" type="text/javascript"></script>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Publier une recette</title>
 </head>
+
 <body>
-    <?php
-    include_once("../database.php");
-    session_start();
-
-    // Vérification si l'utilisateur est connecté
-    if (!isset($_SESSION['idUser'])) {
-        // Rediriger l'utilisateur vers la page de connexion
-        header("Location: ../profil/login.php");
-        exit();
-    }
-
-    // Vérification si l'utilisateur est l'administrateur
-    if ($_SESSION['idUser'] === 'admin') {
-        // Rediriger l'administrateur vers la page de consultation des recettes
-        header("Location: consulteRecipe.php");
-        exit();
-    }
-
-    // Traitement du formulaire de création de recette
-    if (isset($_POST['submit'])) {
-        // Récupération des données du formulaire
-        $titre = $_POST['name'];
-        $description = $_POST['description'];
-        //$prix = $_POST['prix'];
-        $personnes = $_POST['personnes'];
-        $temps = $_POST['temps'];
-        $difficulte = $_POST['difficulte'];
-
-        // Traitement de l'image
-        $image = $_FILES['image'];
-        $imagePath = uploadImage($image);
-
-        
-        // Requête SQL pour insérer la recette dans la table "recipe"
-        $requeteInsertion = "INSERT INTO recipe (name, description, image, idUser, fornumber, time, difficulty)
-                             VALUES ('$titre', '$description', '$imagePath','".$_SESSION['idUser']."', $personnes, '$temps', '$difficulte')";
-
-        // Exécution de la requête d'insertion
-        if (mysqli_query($connexion, $requeteInsertion)) {
-            // Rediriger l'utilisateur vers la page d'accueil avec un message de succès
-            alert('La recette a été envoyé avec succès');
-            //header("Location: home.php?success=1");
-            //exit();
-        } else {
-            echo "Erreur lors de l'insertion de la recette : " . mysqli_error($connexion);
-        }
-
-        // Récupérer l'ID de la recette insérée
-        $recipeId = $pdo->lastInsertId();
-
-        // Insérer les ingrédients de la recette dans la table "product"
-        $ingredients = $_POST['ingredient_name'];
-        $quantities = $_POST['ingredient_quantity'];
-
-        for ($i = 0; $i < count($ingredients); $i++) {
-            $ingredient = $ingredients[$i];
-            $quantity = $quantities[$i];
-            $sqluu = "INSERT INTO product (idRecipe, name, amount) VALUES ($recipeId, $ingredient, $quantity)";
-            $resulta_sqluu = mysqli_query($connexion, $sqluu);
-        }
-
-        // Calculer et mettre à jour le prix dans la table "recipeinprocess"
-        $sql = "UPDATE recipeinprocess SET price = (
-            SELECT SUM(p.price * ri.amount)
-            FROM product ri
-            JOIN productlist p ON ri.name = p.name
-            WHERE ri.idRecipe = $recipeId
-        ) WHERE id = $recipeId";
-        $resulta_sql = mysqli_query($connexion, $sql);
-        // Succès de l'enregistrement
-        echo "<script>alert('L'envoie s'est faite avec succès!');</script>";
-        // Redirection vers une page de confirmation ou une autre action souhaitée
-        if (isset($_SESSION['idUser'])) {
-            echo "<script>window.location.href = '../accueil/home.php?success=1';</script>";
-        } else {
-            echo "<script>window.location.href = '../accueil/home.php?success=0';</script>";
-        }
-
-        // Fermeture de la connexion à la base de données
-        mysqli_close($connexion);
-    }
-    else{
-        echo "<script>alert('Une erreur est survenue lors de l'enregistrement de la publication. Veuillez réessayer.');</script>";
-
-    }
-
-    // Fonction pour gérer le téléchargement et l'enregistrement de l'image
-    function uploadImage($image) {
-        // Vérifier si le téléchargement de l'image a réussi
-        if ($image['error'] === 0) {
-            // Chemin de destination de l'image (dossier "uploads" avec un nom de fichier unique)
-            $destination = "../../uploads/" . uniqid() . "_" . $image['name'];
-
-            // Déplacer l'image téléchargée vers le dossier de destination
-            move_uploaded_file($image['tmp_name'], $destination);
-
-            // Retourner le chemin de l'image
-            return $destination;
-        } else {
-            echo "Erreur lors du téléchargement de l'image.";
-            return "";
-        }
-    }
-    ?>
-
     <header>
         <div id="top">
             <?php
@@ -136,114 +33,115 @@
     </header>
 
     <div class="center">
-    <h1>Publier une recette</h1>
-</div>
+        <h1>Publier une recette</h1>
+    </div>
 
-<div class="recette-details" id="content">
-    <form method="POST" enctype="multipart/form-data">
-        <label for="name">Nom de la recette :</label>
-        <input type="text" name="name" required><br>
+    <div class="recette-details" id="content">
+        <form method="POST" action="insertRecipe.php" enctype="multipart/form-data">
+            <label for="name">Nom de la recette :</label>
+            <input type="text" name="name" required><br>
 
-        <label for="description">Description :</label>
-        <textarea name="description" required rows="7" cols="70"></textarea><br>
+            <label for="description">Description :</label>
+            <textarea name="description" required rows="7" cols="70"></textarea><br>
 
-        <label for="image">Image :</label>
-        <input type="file" name="image"><br>
+            <label for="image">Image :</label>
+            <input type="file" name="image"><br>
 
-        <label for="fornumber">Pour combien de personnes :</label>
-        <input type="number" name="fornumber" required><br>
+            <label for="fornumber">Pour combien de personnes :</label>
+            <input type="number" name="fornumber" required><br>
 
-        <label for="time">Temps de préparation :</label>
-        <input type="text" name="time" required required placeholder="en heure"><br>
+            <label for="time">Temps de préparation :</label>
+            <input type="text" name="time" required required placeholder="en heure"><br>
 
-        <label for="difficulty">Difficulté :</label>
-        <select name="difficulty" required placeholder="difficulty">
-            <option value='facile'> facile</option>
-            <option value='moyen'> moyen</option>
-            <option value='difficile'> difficile</option>
-        </select>
+            <label for="difficulty">Difficulté :</label>
+            <select name="difficulty" required placeholder="difficulty">
+                <option value='facile'> facile</option>
+                <option value='moyen'> moyen</option>
+                <option value='difficile'> difficile</option>
+            </select>
 
-        <div id="ingredients-container">
-            <div class="ingredient-row">
-                <select name="ingredient_name[]">
-                    <?php
-                    // Connexion à la base de données pour récupérer la liste des ingrédients
-                    $pdo = new PDO("mysql:host=localhost;dbname=website_database", "projetRecdevweb", "projetRecdevweb2023");
-                    $sql = "SELECT name FROM productlist";
-                    $stmt = $pdo->query($sql);
-                    $ingredients = $stmt->fetchAll(PDO::FETCH_COLUMN);
-                    foreach ($ingredients as $ingredient) {
-                        echo "<option value='$ingredient'>$ingredient</option>";
-                    }
-                    ?>
-                </select>
-                <input type="text" name="ingredient_quantity[]" required placeholder="Quantité*">
-                <button type="button" class="remove-ingredient">Supprimer</button>
+            <div id="ingredients-container">
+                <div class="ingredient-row">
+                    <select name="ingredient_name[]">
+                        <?php
+                        // Connexion à la base de données pour récupérer la liste des ingrédients
+                        $pdo = new PDO("mysql:host=localhost;dbname=website_database", "projetRecdevweb", "projetRecdevweb2023");
+                        $sql = "SELECT name FROM productlist";
+                        $stmt = $pdo->query($sql);
+                        $ingredients = $stmt->fetchAll(PDO::FETCH_COLUMN);
+                        foreach ($ingredients as $ingredient) {
+                            echo "<option value='$ingredient'>$ingredient</option>";
+                        }
+                        ?>
+                    </select>
+                    <input type="text" name="ingredient_quantity[]" required placeholder="Quantité*">
+                    <button type="button" class="remove-ingredient">Supprimer</button>
+                </div>
             </div>
-        </div>
 
-        <button type="button" id="add-ingredient">Ajouter un ingrédient</button>
-        
-        <input type="submit" value="Envoyer à un administrateur"></br>
-        <label><small>Quantité : Les quantités sont valorisées sur le prix :
-            </br>- Au kilos pour les produits solides
-            </br>- Au litre pour les produits liquides
-            </br>- Au prix unitaire pour les produits élémentaires
+            <button type="button" id="add-ingredient">Ajouter un ingrédient</button>
+
+            <input type="submit" value="Envoyer à un administrateur"></br>
+            <label><small>Quantité : Les quantités sont valorisées sur le prix :
+                    </br>- Au kilos pour les produits solides
+                    </br>- Au litre pour les produits liquides
+                    </br>- Au prix unitaire pour les produits élémentaires
                 </small> </label></br>
-    </form>
-    
-</div>
+        </form>
+
+    </div>
     <footer>
         <div>
             <?php
-                
-                if (isset($_SESSION['idUser'])) {
-                    echo ' <a href="../accueil/home.php?success=1">← Retour</a>';
 
-                } else {
-                    echo '<a href="../accueil/home.php?success=0">Connexion</a>';
-                }
-                ?>
-                
-            </div>
+            if (isset($_SESSION['idUser'])) {
+                echo ' <a href="../accueil/home.php?success=1">← Retour</a>';
+
+            } else {
+                echo '<a href="../accueil/home.php?success=0">Connexion</a>';
+            }
+            ?>
+
+        </div>
     </footer>
     <script>
-    window.addEventListener('DOMContentLoaded', () => {
-        const addIngredientBtn = document.getElementById('add-ingredient');
-        const ingredientsContainer = document.getElementById('ingredients-container');
+        window.addEventListener('DOMContentLoaded', () => {
+            const addIngredientBtn = document.getElementById('add-ingredient');
+            const ingredientsContainer = document.getElementById('ingredients-container');
 
-        addIngredientBtn.addEventListener('click', () => {
-            const ingredientRow = document.createElement('div');
-            ingredientRow.className = 'ingredient-row';
+            addIngredientBtn.addEventListener('click', () => {
+                const ingredientRow = document.createElement('div');
+                ingredientRow.className = 'ingredient-row';
 
-            const select = document.createElement('select');
-            select.name = 'ingredient_name[]';
-            <?php
+                const select = document.createElement('select');
+                select.name = 'ingredient_name[]';
+                <?php
                 foreach ($ingredients as $ingredient) {
                     echo "select.innerHTML += '<option value=\"$ingredient\">$ingredient</option>';\n";
                 }
-            ?>
+                ?>
 
-            const input = document.createElement('input');
-            input.type = 'text';
-            input.name = 'ingredient_quantity[]';
-            input.required = true;
+                const input = document.createElement('input');
+                input.type = 'text';
+                input.name = 'ingredient_quantity[]';
+                input.required = true;
 
-            const removeBtn = document.createElement('button');
-            removeBtn.type = 'button';
-            removeBtn.className = 'remove-ingredient';
-            removeBtn.textContent = 'Supprimer';
-            removeBtn.addEventListener('click', () => {
-                ingredientRow.remove();
+                const removeBtn = document.createElement('button');
+                removeBtn.type = 'button';
+                removeBtn.className = 'remove-ingredient';
+                removeBtn.textContent = 'Supprimer';
+                removeBtn.addEventListener('click', () => {
+                    ingredientRow.remove();
+                });
+
+                ingredientRow.appendChild(select);
+                ingredientRow.appendChild(input);
+                ingredientRow.appendChild(removeBtn);
+
+                ingredientsContainer.appendChild(ingredientRow);
             });
-
-            ingredientRow.appendChild(select);
-            ingredientRow.appendChild(input);
-            ingredientRow.appendChild(removeBtn);
-
-            ingredientsContainer.appendChild(ingredientRow);
         });
-    });
-</script>
+    </script>
 </body>
+
 </html>
